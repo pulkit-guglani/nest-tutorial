1f172fa66c9d42d2bc8d78ac9e4d5e229b77505a
pulkit-guglani <pulkitname@gmail.com>
2025-02-12 17:46:30 +0530

    test: enhance end-to-end tests for authentication and user management

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,9 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-
+import { PrismaService } from '../src/prisma/prisma.service';
+import * as pactum from 'pactum';
+import { AuthDto } from 'src/auth/dto';
 describe('App end to end', () => {
   let app: INestApplication;
   let prisma: PrismaService;
@@ -15,12 +16,44 @@ describe('App end to end', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(3333);
 
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+    pactum.request.setBaseUrl('http:///localhost:3333');
   });
   afterAll(() => {
     app.close();
   });
-  it.todo('should pass');
+  // it.todo('should pass');
+
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it('should Signup', () => {
+        const dto: AuthDto = {
+          email: 'test99@gmail.com',
+          password: 'test',
+        };
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+    describe('Signin', () => {});
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {});
+    describe('Edit user', () => {});
+  });
+
+  describe('Bookmark', () => {
+    describe('Create bookmark', () => {});
+    describe('Get bookmarks', () => {});
+    describe('Get bookmark by Id', () => {});
+    describe('Edit bookmarks', () => {});
+    describe('Delete bookmarks', () => {});
+  });
 });
